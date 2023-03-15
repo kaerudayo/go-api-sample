3d040d7ebd4651baebc6dc7168ba7e1c959848a5
kaeru <soccerxxkaeru@gmail.com>
2023-03-16 01:03:03 +0900

    once commit

--- app/main.go
+++ app/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/api-sample/app/consts"
 	"github.com/api-sample/app/router"
 )
 
 func main() {
 	e := router.NewRouter()
+	fmt.Println(consts.APIPort)
 	e.Logger.Fatal(e.Start(":" + consts.APIPort))
 }
