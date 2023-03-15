5b9fcdb05622b9117e18fc3c5629d365b9988c35
kaeru <soccerxxkaeru@gmail.com>
2023-03-15 23:08:06 +0900

    handlerを追加

--- app/main.go
+++ app/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/api-sample/app/consts"
-	"github.com/labstack/echo/v4"
+	"github.com/api-sample/app/router"
 )
 
 func main() {
-	// インスタンスを作成
-	e := echo.New()
-
-	e.GET("/", hello)
-	fmt.Println(consts.APIPort)
+	e := router.NewRouter()
 
 	e.Logger.Fatal(e.Start(":" + consts.APIPort))
-}
-
-// ハンドラーを定義
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
