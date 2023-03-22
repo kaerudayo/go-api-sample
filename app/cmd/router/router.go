cee591972378f23aa0c4e7f0b5b0016d0fcdadec
kaeru <soccerxxkaeru@gmail.com>
2023-03-23 00:31:10 +0900

    routerを移動

---
+++ app/cmd/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/api-sample/app/pkg/logger"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func NewRouter() *echo.Echo {
+	e := echo.New()
+	e.Use(logger.LoggerMiddleware())
+	e.Use(middleware.Recover())
+
+	NewUserRoute(e)
+	return e
+}
